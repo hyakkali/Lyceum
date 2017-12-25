49233e0364245f2f56baa0ede71a13a530cf3fe4
Hemanth Yakkali <hyakkali@Hemanths-MacBook-Pro.local>
2017-12-24 21:17:59 -0800

    Added login_required page

--- server/middleware/loginRequired.js
+++ server/middleware/loginRequired.js
@@ -3,11 +3,8 @@ var {User} = require('./../models/user');
 var requiresLogin = (req,res,next)=>{
   if (req.session && req.session.userId) {
     return next();
-  }else {
-    var err = new Error('You must be logged in to perform this action.');
-    err.status = 401;
-    return next(err);
   }
+  res.render('login_required.hbs');
 }
 
 var isAuthenticated = (req,res,next)=>{
