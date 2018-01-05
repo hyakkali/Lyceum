3b6a6eb5caf92bb08fc512d5f5c7eae07550cff9
Hemanth Yakkali <hyakkali@Hemanths-MacBook-Pro.local>
2018-01-05 15:19:37 -0800

    Configured mongoose connection to mLab

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,8 @@ var env = process.env.NODE_ENV||'development';
 
 if (env==='development') {
   process.env.PORT=3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/Lyceum';
+  // process.env.MONGODB_URI = 'mongodb://localhost:27017/Lyceum';
+  process.env.MONGODB_URI = 'mongodb://hyakkali:XLGXf6WBTErBarre@ds141657.mlab.com:41657/lyceummvp';
 }else if (env==='test') {
   process.env.PORT=3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/LyceumTest'
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
+// mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI,{
+  useMongoClient:true
+});
+
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 module.exports = {mongoose};
